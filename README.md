468eb2d24dbe77cf993fe35ec56a60ea83cff9b0
Pawel Wojcik <pawel.wojcik5@gmail.com>
2018-09-11 22:49:51 +0200

    New bytes of .wav read. Surprise at the end.

--- README.md
+++ README.md
@@ -8,15 +8,17 @@ First one is .wav. Specification that I realy on is available here http://soundf
 The first four bytes of .wav file are 4 ASCII letters RIFF.
 I read them with the use of ifsteam (http://www.cplusplus.com/doc/tutorial/files/).
 
-#include <fstream>
+	#include <fstream>
 
-char* RIFF = new char[4];
+	char* RIFF = new char[4];
 
-ifstream wave(file_name, ios::binary);
-if( wave.is_open() )
-{
-	for( int i=0; i < 4; i++ )
-		wave >> RIFF[i];
-}
+	ifstream wave(file_name, ios::binary);
+	if( wave.is_open() )
+	{
+		for( int i=0; i < 4; i++ )
+			wave >> RIFF[i];
+	}
 
+Here is a reportedly good approach to read bytes:
+https://en.cppreference.com/w/cpp/iterator/istreambuf_iterator
 
--- riff.cpp
+++ riff.cpp
@@ -8,18 +8,24 @@ const string file_name_0 = "383940.wav";
 const string file_name_1 = "../Klocuch/klocych.wav";
 
 
-bool read_RIFF(ifstream& wave, bool print = true)
+bool read_characters(ifstream& wave, int how_many,
+		string label, bool print = true)
 {
-	char RIFF[4];
-	for(int i=0; i<4; i++)
-		wave >> RIFF[i];
+	char* characters = new char[how_many];
+	for(int i=0; i< how_many; i++)
+		wave >> characters[i];
 
 	if (print)
-		cout << "Chunk ID:\t" << RIFF << endl;
+		cout << label 
+			<<":\t" << characters << endl;
 
 	return wave.good();
 }
 
+
+/* 
+ * Theese functions read the "RIFF" chunk descriptior 
+ */
 
 uint32_t get_file_size(ifstream& wave)
 {
@@ -46,17 +52,42 @@ uint32_t get_file_size(ifstream& wave)
 	return file_size;
 }
 
-bool read_WAVE(ifstream& wave, bool print = true)
+
+uint32_t get_four_byte_int(ifstream& wave)
 {
-	char* WAVE = new char[4];
-	for(int i=0; i<4; i++)
-		wave >> WAVE[i];
+	uint8_t* input = new uint8_t[4];
 
-	if (print)
-		cout << "WAVE:\t\t" << WAVE << endl;
+	for(int i=0; i < 4; i++)
+		wave >> input[i];
 
-	return wave.good();
+	uint32_t number = 0;
+    number  = input[3] << 24;
+	number |= input[2] << 16;
+   	number |= input[1] << 8;
+   	number |= input[0];
+
+	return number;
 }
+
+
+uint16_t get_two_byte_int(ifstream& wave)
+{
+	uint8_t* input = new uint8_t[2];
+
+	for(int i=0; i < 2; i++)
+		wave >> input[i];
+
+	uint16_t number = 0;
+   	number |= input[1] << 8;
+   	number |= input[0];
+
+	return number;
+}
+
+
+/* 
+ * Theese functions read the "fmt" sub-chunk
+ */
 
 int main(int argc, char** argv){
 	string file_name;
@@ -72,16 +103,102 @@ int main(int argc, char** argv){
 	ifstream is( file_name, ios::binary );
 	if( is.is_open() )
 	{
-		uint32_t file_size;
 		is >> noskipws;
-		read_RIFF(is);
+		bool all_good = true;
+
+		// "RIFF" chunk descriptor data
+		uint32_t file_size;
+		all_good &= read_characters(is, 4, "ChunkID");
 		file_size = get_file_size(is);
 		cout << "File size:\t" << file_size << endl;
-		read_WAVE(is);
+		all_good &= read_characters(is, 4, "Format\t");
+		file_size -= 4;
+
+		// "fmt" sub-chunk 
+		cout << endl << endl;
+		all_good &= read_characters(is, 4, "Subchunk1ID");
+		file_size -= 4;
+
+		uint32_t subchunk1_size = get_four_byte_int(is);
+		cout << "Subchunk1 size:\t" << subchunk1_size << endl;
+		file_size -= 4;
+
+		uint16_t audio_format = get_two_byte_int(is);
+		cout << "Audio format:\t" << audio_format << endl;
+		file_size -= 2;
+		subchunk1_size -= 2;
+		
+		uint16_t num_channels = get_two_byte_int(is);
+		cout << "No of Channels:\t" << num_channels << endl;
+		file_size -= 2;
+		subchunk1_size -= 2;
+
+		uint32_t sample_rate = get_four_byte_int(is);
+		cout << "Sample rate:\t" << sample_rate << endl;
+		file_size -= 4;
+		subchunk1_size -= 4;
+
+		// byte_rate = SampleRate * NumChannels * BitsPerSample/8
+		uint32_t byte_rate = get_four_byte_int(is);
+		cout << "Byte rate:\t" << byte_rate << endl;
+		file_size -= 4;
+		subchunk1_size -= 4;
+
+		// block_align = Num_channels * BitsPerSample/8
+		uint16_t block_align = get_two_byte_int(is);
+		cout << "Block align:\t" << block_align << endl;
+		file_size -= 2;
+		subchunk1_size -= 2;
+
+		uint16_t bits_per_sample = get_two_byte_int(is);
+		cout << "Bits per sample:\t" << bits_per_sample << endl;
+		file_size -= 2;
+		subchunk1_size -= 2;
+
+		cout << "Remaining size of sub-chunk 1:\t" 
+			<< subchunk1_size << endl << endl << endl;
+
+		// "fmt" sub-chunk 
+		all_good &= read_characters(is, 4, "Subchunk2ID");
+		file_size -= 4;
+
+		uint32_t subchunk2_size = get_four_byte_int(is);
+		cout << "Sub-chunk 2 size:\t" << subchunk2_size << endl;
+		file_size -= 4;
+		cout << "remaining file size:\t" << file_size << endl;
+
 		is.close();
 	}
 	else 
 		cout << "Cannot open " << file_name << endl;
 
 	return 0;
+}
+
+
+/*
+ * Deprecated function
+ */
+bool read_RIFF(ifstream& wave, bool print = true)
+{
+	char RIFF[4];
+	for(int i=0; i<4; i++)
+		wave >> RIFF[i];
+
+	if (print)
+		cout << "Chunk ID:\t" << RIFF << endl;
+
+	return wave.good();
+}
+
+bool read_WAVE(ifstream& wave, bool print = true)
+{
+	char* WAVE = new char[4];
+	for(int i=0; i<4; i++)
+		wave >> WAVE[i];
+
+	if (print)
+		cout << "WAVE:\t\t" << WAVE << endl;
+
+	return wave.good();
 }
