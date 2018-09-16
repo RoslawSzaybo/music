5c2032a8a1cc6facc004f8f5cdfef2dd79b2b51d
Pawel Wojcik <pawel.wojcik5@gmail.com>
2018-09-16 22:01:10 +0200

    Data is available. Not its time to understand what does it say.

--- riff.cpp
+++ riff.cpp
@@ -1,3 +1,4 @@
+/* RIFF stands for Resource Interchangange File Format */
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,7 +7,6 @@ using namespace std;
 
 const string file_name_0 = "383940.wav";
 const string file_name_1 = "../Klocuch/klocych.wav";
-
 
 bool read_characters(ifstream& wave, int how_many,
 		string label, bool print = true)
@@ -22,36 +22,28 @@ bool read_characters(ifstream& wave, int how_many,
 	return wave.good();
 }
 
-
-/* 
- * Theese functions read the "RIFF" chunk descriptior 
- */
-
-uint32_t get_file_size(ifstream& wave)
+bool read_characters_and_compare(ifstream& wave, int how_many,
+		string label, string pattern,  bool print = true)
 {
-	/* for 383940.wav this bytes are
-	11011100 11011000 00001100 00000000
- 	*/
-	uint8_t* input = new uint8_t[4];
+	char* characters = new char[how_many];
+	for(int i=0; i< how_many; i++)
+		wave >> characters[i];
 
-	for(int i=0; i < 4; i++)
-		wave >> input[i];
+	if (print)
+		cout << label 
+			<<":\t" << characters << endl;
 
-	/* debug
-	for(int i=0; i <4; i++)
-		cout << bitset<8>(input[3-i]) << "\t";
-	cout << endl;
-	*/
+	bool characters_same_as_pattern = true;
+	int len_of_pattern = pattern.length();
+	for(int i=0; i < len_of_pattern; i++)
+		if( pattern[i] != characters[i]) 
+		{
+			characters_same_as_pattern = false;
+			break;
+		}
 
-	int32_t file_size = 0;
-    file_size  = input[3] << 24;
-	file_size |= input[2] << 16;
-   	file_size |= input[1] << 8;
-   	file_size |= input[0];
-
-	return file_size;
+	return characters_same_as_pattern;
 }
-
 
 uint32_t get_four_byte_int(ifstream& wave)
 {
@@ -69,7 +61,6 @@ uint32_t get_four_byte_int(ifstream& wave)
 	return number;
 }
 
-
 uint16_t get_two_byte_int(ifstream& wave)
 {
 	uint8_t* input = new uint8_t[2];
@@ -85,16 +76,22 @@ uint16_t get_two_byte_int(ifstream& wave)
 }
 
 
-/* 
- * Theese functions read the "fmt" sub-chunk
- */
+/* Main starts here */
+/* Main starts here */
+/* Main starts here */
+/* Main starts here */
 
 int main(int argc, char** argv){
+	/* pick one of two slightly different files */
 	string file_name;
-	if( argc == 2)
+	if( argc > 1)
 		file_name = file_name_0;
 	else
 		file_name = file_name_1;
+
+	bool print_data = true;
+	if(argc == 3)
+		print_data = false;
 
 	cout << "Dear sound!" << endl;
 	cout << "Here I display the content of " << file_name << endl;
@@ -109,7 +106,7 @@ int main(int argc, char** argv){
 		// "RIFF" chunk descriptor data
 		uint32_t file_size;
 		all_good &= read_characters(is, 4, "ChunkID");
-		file_size = get_file_size(is);
+		file_size = get_four_byte_int(is);
 		cout << "File size:\t" << file_size << endl;
 		all_good &= read_characters(is, 4, "Format\t");
 		file_size -= 4;
@@ -158,14 +155,49 @@ int main(int argc, char** argv){
 		cout << "Remaining size of sub-chunk 1:\t" 
 			<< subchunk1_size << endl << endl << endl;
 
-		// "fmt" sub-chunk 
-		all_good &= read_characters(is, 4, "Subchunk2ID");
+		// "data" sub-chunk 
+		bool supported = read_characters_and_compare(is, 4, "Subchunk2ID",
+				"data");
 		file_size -= 4;
+
+		if(!supported)
+		{
+			cout << "The file format is not supported." << endl;
+			cout << "Only data with Subchunk2ID equal to data is supported"
+				<< endl;
+			return 0;
+		}
 
 		uint32_t subchunk2_size = get_four_byte_int(is);
 		cout << "Sub-chunk 2 size:\t" << subchunk2_size << endl;
 		file_size -= 4;
 		cout << "remaining file size:\t" << file_size << endl;
+
+		cout << endl << endl;
+		cout << "Now there will be printed all data" << endl;
+		cout << "Sample No\t left channel\tright channel" << endl;
+
+		int data_samples = subchunk2_size/num_channels/bits_per_sample*8;
+		if(bits_per_sample == 16 && num_channels == 2)
+		{
+			uint16_t left_channel;
+			uint16_t right_channel;
+			for(int j = 0; j < data_samples; j++)
+			{
+				left_channel =  get_two_byte_int(is);
+				right_channel =  get_two_byte_int(is);
+				if(print_data)
+					cout << j << "\t"
+						<< bitset<16>(left_channel) << "\t"
+						<< bitset<16>(right_channel) << endl;
+			}
+			file_size -= 4*data_samples;
+			subchunk2_size -= 4*data_samples;
+			cout << endl << endl << "End of data" << endl;
+			cout << "remaining sub-chunk 2 size:\t" 
+				<< subchunk2_size << endl;
+			cout << "remaining file size:\t" << file_size << endl;
+		}
 
 		is.close();
 	}
@@ -177,7 +209,7 @@ int main(int argc, char** argv){
 
 
 /*
- * Deprecated function
+ * Deprecated functions
  */
 bool read_RIFF(ifstream& wave, bool print = true)
 {
@@ -201,4 +233,29 @@ bool read_WAVE(ifstream& wave, bool print = true)
 		cout << "WAVE:\t\t" << WAVE << endl;
 
 	return wave.good();
+}
+
+uint32_t get_file_size(ifstream& wave)
+{
+	/* for 383940.wav this bytes are
+	11011100 11011000 00001100 00000000
+ 	*/
+	uint8_t* input = new uint8_t[4];
+
+	for(int i=0; i < 4; i++)
+		wave >> input[i];
+
+	/* debug
+	for(int i=0; i <4; i++)
+		cout << bitset<8>(input[3-i]) << "\t";
+	cout << endl;
+	*/
+
+	int32_t file_size = 0;
+    file_size  = input[3] << 24;
+	file_size |= input[2] << 16;
+   	file_size |= input[1] << 8;
+   	file_size |= input[0];
+
+	return file_size;
 }
